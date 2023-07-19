197baeb036c91844e32d69a14100f4e3fc49d94e
Naranbat <massive0304@gmail.com>
2023-07-19 17:08:13 +0800

    feat: eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   root: true,
-  extends: "@react-native",
+  extends: '@react-native',
   rules: {
-    "@typescript-eslint/no-non-null-assertion": "warn",
+    '@typescript-eslint/no-non-null-assertion': 'warn',
     // RN 0.70.0 introduces new JSX transform, so that React namespace import is not required
-    "react/react-in-jsx-scope": "off",
+    'react/react-in-jsx-scope': 'off',
   },
 };
